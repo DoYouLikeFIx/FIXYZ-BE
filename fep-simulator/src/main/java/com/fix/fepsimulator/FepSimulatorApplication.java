9e87f1c5ba25a31018fddd40b83cc7d47f6f02da
yeomin4242 <whereismysejong@naver.com>
2026-03-02 12:54:08 +0900

    Feat: add fep-simulator module with OpenAPI support, error handling, and internal secret filter FIX-27

---
+++ fep-simulator/src/main/java/com/fix/fepsimulator/FepSimulatorApplication.java
@@ -0,0 +1,12 @@
+package com.fix.fepsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FepSimulatorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FepSimulatorApplication.class, args);
+  }
+}
