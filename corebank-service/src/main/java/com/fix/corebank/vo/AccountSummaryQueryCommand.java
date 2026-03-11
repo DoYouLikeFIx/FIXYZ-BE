0d1e5eefb79ddef1835e18120754e3b1df7469dd
yeomin4242 <whereismysejong@naver.com>
2026-03-11 23:32:06 +0900

    Feat: add AccountPositionsQueryCommand and AccountSummaryQueryCommand classes

---
+++ corebank-service/src/main/java/com/fix/corebank/vo/AccountSummaryQueryCommand.java
@@ -0,0 +1,24 @@
+package com.fix.corebank.vo;
+
+public class AccountSummaryQueryCommand {
+
+  private final Long accountId;
+  private final Long memberId;
+
+  private AccountSummaryQueryCommand(Long accountId, Long memberId) {
+    this.accountId = accountId;
+    this.memberId = memberId;
+  }
+
+  public static AccountSummaryQueryCommand of(Long accountId, Long memberId) {
+    return new AccountSummaryQueryCommand(accountId, memberId);
+  }
+
+  public Long getAccountId() {
+    return accountId;
+  }
+
+  public Long getMemberId() {
+    return memberId;
+  }
+}
