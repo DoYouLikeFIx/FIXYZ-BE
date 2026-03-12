e1ca104829d82d2b9390ee2059fa93dee56f9d1e
민영재 <whereismysejong@naver.com>
2026-03-12 19:50:32 +0900

    Fix: update OrderSessionQueryCommand to include clOrdId in constructor and factory method

--- channel-service/src/main/java/com/fix/channel/vo/OrderSessionQueryCommand.java
+++ channel-service/src/main/java/com/fix/channel/vo/OrderSessionQueryCommand.java
@@ -4,14 +4,16 @@ public class OrderSessionQueryCommand {
 
   private final Long memberId;
   private final String orderSessionId;
+  private final String clOrdId;
 
-  private OrderSessionQueryCommand(Long memberId, String orderSessionId) {
+  private OrderSessionQueryCommand(Long memberId, String orderSessionId, String clOrdId) {
     this.memberId = memberId;
     this.orderSessionId = orderSessionId;
+    this.clOrdId = clOrdId;
   }
 
-  public static OrderSessionQueryCommand of(Long memberId, String orderSessionId) {
-    return new OrderSessionQueryCommand(memberId, orderSessionId);
+  public static OrderSessionQueryCommand of(Long memberId, String orderSessionId, String clOrdId) {
+    return new OrderSessionQueryCommand(memberId, orderSessionId, clOrdId);
   }
 
   public Long getMemberId() {
@@ -20,5 +22,9 @@ public class OrderSessionQueryCommand {
 
   public String getOrderSessionId() {
     return orderSessionId;
+  }
+
+  public String getClOrdId() {
+    return clOrdId;
   }
 }
