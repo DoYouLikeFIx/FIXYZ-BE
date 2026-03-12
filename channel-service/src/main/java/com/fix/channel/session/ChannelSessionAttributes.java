da595b1244f6b085c00c3a07854191c22df5f80e
민영재 <whereismysejong@naver.com>
2026-03-12 19:50:27 +0900

    Fix: add AUTH_ACCOUNT_ID and AUTH_ACCOUNT_NUMBER constants to ChannelSessionAttributes

--- channel-service/src/main/java/com/fix/channel/session/ChannelSessionAttributes.java
+++ channel-service/src/main/java/com/fix/channel/session/ChannelSessionAttributes.java
@@ -4,6 +4,8 @@ public final class ChannelSessionAttributes {
 
   public static final String AUTH_MEMBER_ID = "AUTH_MEMBER_ID";
   public static final String AUTH_MEMBER_NAME = "AUTH_MEMBER_NAME";
+  public static final String AUTH_ACCOUNT_ID = "AUTH_ACCOUNT_ID";
+  public static final String AUTH_ACCOUNT_NUMBER = "AUTH_ACCOUNT_NUMBER";
   public static final String AUTH_MFA_VERIFIED_AT = "AUTH_MFA_VERIFIED_AT";
   public static final String AUTH_ORDER_CHALLENGE_BYPASS_ELIGIBLE = "AUTH_ORDER_CHALLENGE_BYPASS_ELIGIBLE";
   public static final String AUTH_LOGIN_AUTHENTICATED_AT = "AUTH_LOGIN_AUTHENTICATED_AT";
