e198c9ed4867aeee0aaf035322c74ee0e10f44bd
yeomin4242 <whereismysejong@naver.com>
2026-03-12 10:19:54 +0900

    Fix: add method to retrieve positions by account ID ordered by symbol

--- corebank-service/src/main/java/com/fix/corebank/repository/PositionRepository.java
+++ corebank-service/src/main/java/com/fix/corebank/repository/PositionRepository.java
@@ -1,8 +1,11 @@
 package com.fix.corebank.repository;
 
 import com.fix.corebank.entity.Position;
+import java.util.List;
 import com.fix.corebank.repository.custom.PositionCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PositionRepository extends JpaRepository<Position, Long>, PositionCustomRepository {
+
+  List<Position> findAllByAccountIdOrderBySymbolAsc(Long accountId);
 }
