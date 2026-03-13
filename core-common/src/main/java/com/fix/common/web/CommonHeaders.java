8030697f1367638c19c758883430c46caf8cf233
yeomin4242 <whereismysejong@naver.com>
2026-03-13 13:09:28 +0900

    Feat: add error codes and headers for MFA recovery process

--- core-common/src/main/java/com/fix/common/web/CommonHeaders.java
+++ core-common/src/main/java/com/fix/common/web/CommonHeaders.java
@@ -5,6 +5,8 @@ public final class CommonHeaders {
   public static final String X_CORRELATION_ID = "X-Correlation-Id";
   public static final String X_INTERNAL_SECRET = "X-Internal-Secret";
   public static final String X_CL_ORD_ID = "X-ClOrdID";
+  public static final String X_MFA_RECOVERY_PROOF = "X-MFA-Recovery-Proof";
+  public static final String X_MFA_RECOVERY_PROOF_EXPIRES_IN = "X-MFA-Recovery-Proof-Expires-In";
 
   private CommonHeaders() {
   }
