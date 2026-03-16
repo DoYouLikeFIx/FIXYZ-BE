45ee09c7065314a87a0ce2ce51aba2fce31e2e17
민영재 <whereismysejong@naver.com>
2026-03-16 17:52:05 +0900

    Refactor: reorder import statements in SimulatorRule for consistency

--- fep-simulator/src/main/java/com/fix/fepsimulator/entity/SimulatorRule.java
+++ fep-simulator/src/main/java/com/fix/fepsimulator/entity/SimulatorRule.java
@@ -1,14 +1,16 @@
 package com.fix.fepsimulator.entity;
 
+import java.time.Instant;
+import java.util.UUID;
+
 import com.fix.common.entity.BaseTimeEntity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.time.Instant;
-import java.util.UUID;
 
 @Entity
 @Table(name = "simulator_rules")
