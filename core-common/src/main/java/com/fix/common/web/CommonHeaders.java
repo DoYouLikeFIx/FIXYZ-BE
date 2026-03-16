cae59890195db0b40cc88cfd6436f907c86c18d6
민영재 <whereismysejong@naver.com>
2026-03-16 15:52:27 +0900

    Feat: add TraceparentSupport class for handling traceparent headers and request tracing

--- core-common/src/main/java/com/fix/common/web/CommonHeaders.java
+++ core-common/src/main/java/com/fix/common/web/CommonHeaders.java
@@ -2,6 +2,7 @@ package com.fix.common.web;
 
 public final class CommonHeaders {
 
+  public static final String TRACEPARENT = "traceparent";
   public static final String X_CORRELATION_ID = "X-Correlation-Id";
   public static final String X_INTERNAL_SECRET = "X-Internal-Secret";
   public static final String X_CL_ORD_ID = "X-ClOrdID";
