63fb3c00c693924cddf362bc5b492c0e16bcd2b6
yeomin4242 <whereismysejong@naver.com>
2026-03-17 20:29:30 +0900

    Feat: reorganize imports and add findByIdAndMemberId method in NotificationRepository

--- channel-service/src/main/java/com/fix/channel/repository/NotificationRepository.java
+++ channel-service/src/main/java/com/fix/channel/repository/NotificationRepository.java
@@ -1,12 +1,17 @@
 package com.fix.channel.repository;
 
-import com.fix.channel.entity.Notification;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fix.channel.entity.Notification;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
   List<Notification> findByMemberId(Long memberId, Pageable pageable);
 
   List<Notification> findByMemberIdAndIdLessThan(Long memberId, Long cursorId, Pageable pageable);
+
+  Optional<Notification> findByIdAndMemberId(Long id, Long memberId);
 }
