7aa053820348b26d4cff444f6f0d2757dfc553be
yeomin4242 <whereismysejong@naver.com>
2026-03-24 12:34:17 +0900

    Feat: Add method to retrieve executions by account ID, symbols, and execution time range

--- corebank-service/src/main/java/com/fix/corebank/repository/ExecutionRepository.java
+++ corebank-service/src/main/java/com/fix/corebank/repository/ExecutionRepository.java
@@ -2,9 +2,19 @@ package com.fix.corebank.repository;
 
 import com.fix.corebank.entity.Execution;
 import com.fix.corebank.repository.custom.ExecutionCustomRepository;
+import java.time.Instant;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExecutionRepository extends JpaRepository<Execution, Long>, ExecutionCustomRepository {
   List<Execution> findAllByAccountIdAndSymbolOrderByExecutedAtAsc(Long accountId, String symbol);
+
+  List<Execution> findAllByAccountIdAndSymbolInOrderBySymbolAscExecutedAtAscIdAsc(Long accountId, List<String> symbols);
+
+  List<Execution> findAllByAccountIdAndSymbolInAndExecutedAtGreaterThanEqualAndExecutedAtLessThanOrderBySymbolAscExecutedAtAscIdAsc(
+      Long accountId,
+      List<String> symbols,
+      Instant executedAtFrom,
+      Instant executedAtTo
+  );
 }
